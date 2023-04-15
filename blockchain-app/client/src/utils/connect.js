e8af2ea3ab15d686ccd724b2a5c6f48fe8c33417
monaou <monaou.freelance@gmail.com>
2023-04-16 03:38:46 +0900

    add contract address

--- blockchain-app/client/src/utils/connect.js
+++ blockchain-app/client/src/utils/connect.js
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
 export const contractABI = abi.abi;
-export const contactAddress = ""
+export const contactAddress = "0x710508e92D552F411D9c0193f45552ab3008C10C"
